8933c787fdd12a56cd588f5aba379f8a3392e955
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-11-19 08:41:00 +0100

    adjust footer height

--- src/Pages/TvSeriesScreen.js
+++ src/Pages/TvSeriesScreen.js
@@ -16,12 +16,11 @@ const TvSeriesScreen = (props) => {
   return (
     <div className={css.tvSeriesScreen}>
       <Banner />
-      <TvSeries title="Top Twenty" fetchUrl={requests.fetchTvseriesTopten} />
+      <TvSeries title="Top Twenty" fetchUrl={requests.fetchTvseriesTopten}  />
       <TvSeries title="Animation" fetchUrl={requests.fetchTvseriesAnimation} />
       <TvSeries title="Fantasy" fetchUrl={requests.fetchTvseriesFantasy} />
       <TvSeries title="Commedy" fetchUrl={requests.fetchTvseriesCommedy} />
       <TvSeries title="Crime" fetchUrl={requests.fetchTvseriesCrime} />
-
       <TvSeries title="Western" fetchUrl={requests.fetchTvseriesWestern} />
       <TvSeries title="Drama" fetchUrl={requests.fetchTvseriesDrama} />
     </div>
